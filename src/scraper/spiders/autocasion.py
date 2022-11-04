3feb1704a833f91d792ddc812060d85fcb764e0c
agr17 <aregueiro1700@gmail.com>
2022-11-04 10:14:02 +0100

    Added images and corrections to Autocasion spider

--- src/scraper/spiders/autocasion.py
+++ src/scraper/spiders/autocasion.py
@@ -25,17 +25,18 @@ class AutocasionSpider(CrawlSpider):
 
         if name_p1 is not None:
 
-            price_cash = response.css('div.precio').css('span::text').get()
-            price_financed = response.css('div.precio.financiado').css('span::text').get()
-
-            name_p2 = response.css('div.bloque.titulo-ficha').css('h1').css('span::text').get()
-
             item = CarItem()
+            
+            # General info
+            name_p2 = response.css('div.bloque.titulo-ficha').css('h1').css('span::text').get()
             item['title'] = name_p1.strip() + " " + name_p2.strip() if name_p2 is not None else name_p1.strip()
+
             item['publisher'] = 'autocasion'
             item['url'] = response.request.url
-            item['price_cash'] = price_cash
+            item['price_cash'] = response.css('div.precio').css('span::text').get()
+            item['image'] = response.css('img.type-image::attr(src)').get()
 
+            price_financed = response.css('div.precio.financiado').css('span::text').get()
             if price_financed is not None:
                 item['price_financed'] = price_financed
 
@@ -50,9 +51,9 @@ class AutocasionSpider(CrawlSpider):
 
             # brand, location and model (blm)
             blm = response.css('ul.breadcrumb').css('span::text').getall()
-            item['brand'] = blm[0]
-            item['location'] = blm[1]
-            item['model'] = blm[2]
+            item['brand'] = blm[-4]
+            item['location'] = blm[-3]
+            item['model'] = blm[-2]
 
             # seats and doors
             key = response.css('ul.tab-spec-1.active > li:not([class^="dimensiones"]) > span::text').getall()
